253dab67314a7618cfa9de7200332eea9efe33d8
John Angelo David <johnangelo.programming@gmail.com>
2026-04-19 12:12:57 +0800

    feat(reviews): add /reviews route

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const FAQ = lazy(() => import('./components/FAQ'));
 const PeptideCalculator = lazy(() => import('./components/PeptideCalculator'));
 const OrderTracking = lazy(() => import('./components/OrderTracking'));
 const ProtocolGuide = lazy(() => import('./components/ProtocolGuide'));
+const ReviewsPage = lazy(() => import('./components/ReviewsPage'));
 
 import { useMenu } from './hooks/useMenu';
 // import { useCOAPageSetting } from './hooks/useCOAPageSetting';
@@ -112,6 +113,7 @@ function App() {
                     <Route path="/calculator" element={<PeptideCalculator />} />
                     <Route path="/track-order" element={<OrderTracking />} />
                     <Route path="/protocols" element={<ProtocolGuide />} />
+                    <Route path="/reviews" element={<ReviewsPage />} />
                     <Route path="/admin" element={<AdminDashboard />} />
                 </Routes>
             </Suspense>
